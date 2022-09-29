68a0fd82aa8d0b6f11ef230114c72d2045b07dd5
alexander <alex.shklyarenko22@gmail.com>
2022-09-29 15:03:16 +0300

    final master

--- public/index.php
+++ public/index.php
@@ -1,7 +1,17 @@
 <?php
 
 
-require_once __DIR__ . '../vendor/autoload.php';
-require_once __DIR__ . '../config/database.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/database.php';
 
-echo 'lesson start';
+
+use Hillel\Models\User;
+
+
+$orders = \Hillel\Models\Order::all();
+
+
+foreach ($orders as $order) {
+    echo $order->price;
+
+}
---
+++ src/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hillel\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function orders(){
+        return $this->belongsToMany(Order::class,'product2order')->withTimestamps();
+    }
+}
